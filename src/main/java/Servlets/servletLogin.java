89a4459fe6963f258275befb9c0343a45d6ff902
LAB-USR-CHIMBOTE <LAB-USR-CHIMBOTE@UTP318188.grupoutp.net>
2024-06-22 11:02:40 -0500

    anuel brr Login hecho

--- src/main/java/Servlets/servletLogin.java
+++ src/main/java/Servlets/servletLogin.java
@@ -4,14 +4,14 @@
  */
 package Servlets;
 
-import javax.servlet.annotation.WebServlet;
+import jakarta.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  *
@@ -62,6 +62,14 @@ public class servletLogin extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+        String usuario = request.getParameter("usuario");
+        String contra = request.getParameter("contrasena");
+        
+        if("Richin".equals(usuario)&& "123".equals(contra)){
+            response.sendRedirect("home.html");
+        }else{
+            response.sendRedirect("index.html");
+        }
     }
 
     /**
